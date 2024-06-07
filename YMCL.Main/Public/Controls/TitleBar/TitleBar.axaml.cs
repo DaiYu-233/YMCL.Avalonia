80227cd7adf47cc3ce021d23bc498f10b71dfb2f
DaiYu <840673183@qq.com>
2024-06-07 22:04:01 +0800

    🎉 初始化提交

---
+++ YMCL.Main/Public/Controls/TitleBar/TitleBar.axaml.cs
@@ -0,0 +1,78 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+
+namespace YMCL.Main.Public.Controls;
+
+public partial class TitleBar : UserControl
+{
+    public TitleBar()
+    {
+        InitializeComponent();
+        CloseButton.Click += CloseButton_Click;
+        MaximizeButton.Click += MaximizeButton_Click;
+        MinimizeButton.Click += MinimizeButton_Click;
+        MoveDragArea.PointerPressed += MoveDragArea_PointerPressed;
+    }
+
+    private void MoveDragArea_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    {
+        if (e.Pointer.Type == PointerType.Mouse)
+        {
+            var control = sender as Grid;
+            if (control != null)
+            {
+                var window = control.GetVisualRoot() as Window;
+                window.BeginMoveDrag(e);
+            }
+        }
+    }
+
+    private void MinimizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            var window = button.GetVisualRoot() as Window;
+            if (window != null)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+    }
+
+    private void MaximizeButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            var window = button.GetVisualRoot() as Window;
+            if (window != null)
+            {
+                if (window.WindowState == WindowState.Maximized)
+                {
+                    window.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    window.WindowState = WindowState.Maximized;
+                }
+            }
+        }
+    }
+
+    private void CloseButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        if (button != null)
+        {
+            var window = button.GetVisualRoot() as Window;
+            if (window != null)
+            {
+                window.Close();
+            }
+        }
+    }
+}
---
+++ YMCL.Main/Views/Initialization/InitializationWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using System.Diagnostics;
+
+namespace YMCL.Main;
+
+public partial class InitializationWindow : Window
+{
+    public InitializationWindow()
+    {
+        InitializeComponent();
+    }
+}

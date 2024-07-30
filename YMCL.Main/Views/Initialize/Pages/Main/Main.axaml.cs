bf9b94ab756e3381ffede5a5f461612508a7d153
DaiYu-233 <840673183@qq.com>
2024-07-30 23:41:20 +0800

    🎨 重写初始化

---
+++ YMCL.Main/Views/Initialize/Pages/Main/Main.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace YMCL.Main.Views.Initialize.Pages.Main;
+
+public partial class Main : UserControl
+{
+    public Main()
+    {
+        InitializeComponent();
+    }
+}
